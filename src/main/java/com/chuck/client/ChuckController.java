c2b24d7ede867209be7ae88fe83802a1e1f16989
sergio.torres.lozano <sergio.torres.lozano@accenture.com>
2018-01-24 16:13:43 +0200

    adding fallback call

--- src/main/java/com/chuck/client/ChuckController.java
+++ src/main/java/com/chuck/client/ChuckController.java
@@ -2,16 +2,20 @@ package com.chuck.client;
 
 import java.io.Serializable;
 import java.lang.invoke.MethodHandles;
+import java.net.URI;
 
+import com.sun.javafx.fxml.builder.URLBuilder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class ChuckController {
@@ -20,7 +24,9 @@ public class ChuckController {
 
 	private ChuckFactClient chuckFactClient;
 
-	ChuckController(ChuckFactClient chuckFactClient) {
+	private RestTemplate restTemplate;
+
+	ChuckController(ChuckFactClient chuckFactClient, RestTemplate restTemplate) {
 		this.chuckFactClient = chuckFactClient;
 	}
 
@@ -28,6 +34,13 @@ public class ChuckController {
 	public @ResponseBody String home() {
 		log.debug("Got a request!");
 		return "Hello World";
+	}
+
+	@HystrixCommand(fallbackMethod = "localFact")
+	@RequestMapping("/chuckJoke")
+	public @ResponseBody ChuckFact findRandomJoke() {
+		log.debug("Got a request!");
+		return restTemplate.getForEntity("getstartedlab_chuck-service", ChuckFact.class).getBody();
 	}
 
 	@HystrixCommand(fallbackMethod = "localFact")
@@ -75,7 +88,7 @@ class ChuckFact implements Serializable {
 
 }
 
-@FeignClient("chuck-service")
+@FeignClient("getstartedlab_chuck-service")
 interface ChuckFactClient {
 
 	@RequestMapping(method = RequestMethod.GET, value = "/chuck")
