7e4b6e75f85b3220e18b967dcbbe549d7e56ab11
sergio.torres.lozano <sergio.torres.lozano@accenture.com>
2018-08-30 19:50:19 +0300

    Updated to Spring boot 2

--- src/main/java/com/chuck/client/ChuckClient.java
+++ src/main/java/com/chuck/client/ChuckClient.java
@@ -7,12 +7,10 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients
-@EnableDiscoveryClient
 @EnableCircuitBreaker
 public class ChuckClient {
 
--- src/main/java/com/chuck/client/ChuckController.java
+++ src/main/java/com/chuck/client/ChuckController.java
@@ -5,7 +5,7 @@ import java.lang.invoke.MethodHandles;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
