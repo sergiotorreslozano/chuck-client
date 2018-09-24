9492b4848110f492c286bc93619370fec5e40635
sergio.torres.lozano <sergio.torres.lozano@accenture.com>
2018-09-24 21:13:49 +0300

    Port change for tests

--- src/test/java/com/chuck/client/ChuckJokesContractTest.java
+++ src/test/java/com/chuck/client/ChuckJokesContractTest.java
@@ -5,26 +5,31 @@ import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.cloud.contract.stubrunner.spring.AutoConfigureStubRunner;
 import org.springframework.cloud.contract.stubrunner.spring.StubRunnerProperties;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.web.client.RestTemplate;
+
 
 @RunWith(SpringRunner.class)
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureStubRunner(ids = {
-		"com.springboot.docker:chuck-service:0.1.0:stubs:8080" }, stubsMode = StubRunnerProperties.StubsMode.LOCAL)
+		"com.springboot.docker:chuck-service:0.1.0:stubs:8080" },
+		stubsMode = StubRunnerProperties.StubsMode.LOCAL)
 public class ChuckJokesContractTest {
 
-	private RestTemplate restTemplate = new RestTemplate();
+	@Autowired
+	private TestRestTemplate restTemplate;
 
 	@Test
 	public void shouldReturnAJoke() {
 
-		ResponseEntity<ChuckFact> response = restTemplate.getForEntity("http://localhost:8080/chuck", ChuckFact.class);
+		ResponseEntity<ChuckFact> response = restTemplate.getForEntity
+					("/chuck", ChuckFact.class);
 		assertTrue(response.getStatusCode().equals(HttpStatus.OK));
 		assertNotNull(response.getBody().getId());
 		assertNotNull(response.getBody().getFact());
